3b968b9eee2d8eb3198078c52f4de433733f05cf
nitinkumar999 <nitinkumar@live.in>
2026-04-08 08:31:29 +0530

    Create Form.js

---
+++ app/components/Form.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Form() {
+  const [result, setResult] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const res = await fetch("/api/chat", {
+      method: "POST",
+    });
+
+    const data = await res.json();
+    setResult(data.reply);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Enter Name" /><br /><br />
+        <input type="date" /><br /><br />
+        <button type="submit">Generate Kundli</button>
+      </form>
+
+      <p style={{ marginTop: 20 }}>{result}</p>
+    </div>
+  );
+}
