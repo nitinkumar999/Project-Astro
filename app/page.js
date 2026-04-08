800f7b9e412446e364a01e9bc9031278b6be8274
nitinkumar999 <nitinkumar@live.in>
2026-04-08 08:37:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ padding: 40, fontFamily: "sans-serif" }}>
       <h1>🔮 Project Astro</h1>
       <p>AI Kundli Generator</p>
+
       <Form />
     </div>
   );
